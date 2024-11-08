40df82e082b388a7f1f47a89352966cf23c7e914
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-11-08 19:13:59 +0300

    fixed

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -9,12 +9,11 @@ import { OAuthProvider } from "node-appwrite";
 export async function signUpWithGithub() {
 	const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
+		`${process.env.NEXT_PUBLIC_APP}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
@@ -24,12 +23,11 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
 	const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
-  
+
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
+		`${process.env.NEXT_PUBLIC_APP}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
