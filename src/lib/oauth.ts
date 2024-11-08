5358b332fde66d47102e45b6ca73a4bf1e82421f
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-11-08 19:30:03 +0300

    fixed

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -8,7 +8,7 @@ import { OAuthProvider } from "node-appwrite";
 export async function signUpWithGithub() {
 	const { account } = await createAdminClient();
 
-  
+   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
 		`${process.env.NEXT_PUBLIC_APP}/oauth`,
