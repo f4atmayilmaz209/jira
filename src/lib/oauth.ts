cf0cb5127131630ff8dbeee0d2050c016295efb9
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-11-08 19:15:38 +0300

    fixed

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -3,7 +3,6 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
